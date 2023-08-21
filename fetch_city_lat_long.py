f8ec6dc900119dd8d9c755f20f2abbadf95ed723
NguyenS <104602469+sangnguyens@users.noreply.github.com>
2023-08-21 14:16:13 +0000

    finish algos

--- fetch_city_lat_long.py
+++ fetch_city_lat_long.py
@@ -4,10 +4,9 @@
 This is a command-line tool that figures out the shortest distance to visit all cities in a list.
 """
 
-import geopy
-import geopy.distance
 import pandas as pd
 from random import shuffle
+import random
 import click
 
 # build a function that takes variable length of strings and returns a list of cities
@@ -50,3 +49,103 @@ def create_cities_dataframe(cities=None):
     df = pd.DataFrame(data, columns=["city", "latitudes", "longitude"])
 
     return df
+
+def tsp(cities_df):
+    """Traveling Salesman Problem using pandas and Geopy"""
+
+    # create a list of cities
+    city_list = cities_df['city'].to_list()
+
+    # shuffle the list to randomize the order of the cities
+    random.shuffle(city_list)
+    print(f"Randomized list of cities: {city_list}")
+
+    # create a list of distances
+    distances_list = []
+
+    # loop through the list of cities
+    for i in range(len(city_list)):
+        # get the current city and next city
+        current_city = cities_df[cities_df['city'] == city_list[i]]
+        next_city = cities_df[cities_df['city'] == city_list[(i + 1) % len(city_list)]]
+
+        # get the distance between the current city and the next city
+        distance = geopy.distance.distance(
+            (current_city['latitudes'].values[0], current_city['longitude'].values[0]),
+            (next_city['latitudes'].values[0], next_city['longitude'].values[0]),
+        ).miles
+        # append the distance to the list of distances
+        distances_list.append(distance)
+
+    # return the sum of the distance list and the city list
+    total_distance = sum(distances_list)
+
+    return total_distance, city_list
+
+def main(count, df=None):
+    """Main function that runs the tsp simulation multiple times and returns the best result"""
+    # create a list to hold the distances
+    distances_list = []
+    # create a list to hold the cities
+    city_list_list = []
+    # loop through the simulation
+    if df is None:
+        cdf = create_cities_dataframe()
+    else:
+        cdf = df
+    for i in range(count): # run the simulation x time
+        # get the distance and cities
+        distance, city_list = tsp(cdf)
+        print(f"Running simulation: {i}: Found total distance: {distance}")
+
+        # append the distance to the list of distances
+        distances_list.append(distance)
+        # append the cities to the list of cities
+        city_list_list.append(city_list)
+    # get the index of the shortest distance
+    shortest_distance_index = distances_list.index(min(distances_list))
+    # print out the shortest distance
+    print("Shortest distance: {}".format(min(distances_list)))
+    # print the cities visited
+    print("Cities visited: {}".format(city_list_list[shortest_distance_index]))
+
+# create click group
+@click.group()
+def cli():
+    """This is a command-line tool that figures out the shortest distance to visit all cities in a list."""
+
+# create click command that takes a variable of arguments of cities passed in
+@cli.command("cities")
+@click.argument("cities", nargs=-1)
+@click.option("--count", default=10, help="The number of times to run the simulation")
+def cities_cli(cities, count):
+    """This is a command-line tool that figures out the shortest distance to visit all cities in a list.
+    Examle: ./fetch_cities_lat_long.py cities "New York" "Knoxville" --count 2
+    """
+    # create a list of cities
+    city_list = my_cities(*cities)
+    # create a pandas dataframe of cities and their latitudes and longitudes
+    cities_df = create_cities_dataframe(city_list)
+    # run the tsp simulation
+    main(count, cities_df)
+
+# add click command that runs the simulation x times
+@cli.command("simulate")        
+@click.option("--count", default=10, help="The number of times to run the simulation")
+def simulate(count):
+    """Run the simulation x times and print the shortest distance and cities visited.
+
+    Example: ./fetch_cities_lat_long.py simulate --count 15
+
+    """
+
+    print(f"Running simulation: {count} times")
+    main(count)
+
+if __name__ == "__main__":
+    cli()
+
+# run the main function
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    main()

10f0a02b8f83bf16509e1bcfa846d2021f757361
NguyenS <104602469+sangnguyens@users.noreply.github.com>
2023-08-21 13:41:43 +0000

    update code

--- fetch_city_lat_long.py
+++ fetch_city_lat_long.py
@@ -15,13 +15,10 @@ def my_cities(*args):
     """Build a list of cities from input"""
     return list(args)
 
-<<<<<<<<<<<<<  ✨ Codeium AI Suggestion  >>>>>>>>>>>>>>
 def create_cities_dataframe(cities=None):
     """Create a pandas dataframe of cities and their latitudes and longitudes"""
+    import geopy.geocoders
 
-+    import pandas as pd
-+    import geopy.geocoders
-+
     if cities is None:
         cities = [
             "New York",
@@ -45,32 +42,11 @@ def create_cities_dataframe(cities=None):
             "Pheonix",
             "San Diego",
         ]
--        
--    # Create a list to hold the latitudes and logitudes
--    latitudes = []
--    longitudes = []
--
--    # loop through the cities list and get the latitudes and longitudes
--    for city in cities:
--        geolocator = geopy.geocoders.Nominatim(user_agent='tsp_pandas')
--        location = geolocator.geocode(city)
--        latitudes.append(location.latitude)
--        longitudes.append(location.longitude)
--
--    # Create a dataframe from the cities, latitudes and longitude
--    df = pd.DataFrame(
--        {
--            "city": cities,
--            "latitudes": latitudes,
--            "logitude": longitudes,
--        }
--    )
-+
-+    geolocator = geopy.geocoders.Nominatim(user_agent='tsp_pandas')
-+
-+    data = [(city, geolocator.geocode(city).latitude, geolocator.geocode(city).longitude) for city in cities]
-+
-+    df = pd.DataFrame(data, columns=["city", "latitudes", "longitude"])
+
+    geolocator = geopy.geocoders.Nominatim(user_agent='tsp_pandas')
+
+    data = [(city, geolocator.geocode(city).latitude, geolocator.geocode(city).longitude) for city in cities]
+
+    df = pd.DataFrame(data, columns=["city", "latitudes", "longitude"])
 
     return df
-<<<<<  bot-2b8c8332-5d21-4374-9afe-73264093067f  >>>>>
